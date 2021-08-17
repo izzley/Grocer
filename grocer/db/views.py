5311cd2b77768c6eec98051015466a021c81abfe
izzley <izzl@protonmail.com>
2021-08-17 20:44:56 +1000

    FEAT: database views

---
+++ grocer/db/views.py
@@ -0,0 +1,38 @@
+import psycopg2 as pg
+import pandas as pd
+from settings.scrapy import DATABASE
+
+# db credentials
+db = DATABASE['database']
+user = DATABASE['username']
+password = DATABASE['password']
+
+class WooliesViews:
+    # psycopg2 connect
+    conn = pg.connect(f"dbname={db} user={user} password={password}")
+    cur = conn.cursor()
+
+    def current_savings(self):
+        """
+        fetchall latest savings prices. Excludes null savingsamount
+        """
+        query = ('''
+            WITH p AS ( 
+            SELECT displayname, price, savingsamount, instorewasprice, scrapetime, round(CAST(savingsamount/price*100 AS numeric), 2) AS "percent" 
+            FROM woolies_test) 
+            SELECT displayname, price, instorewasprice as wasprice, savingsamount as saving 
+            FROM p 
+            WHERE percent > 0 AND scrapetime = (SELECT MAX(scrapetime) from woolies_test);
+            ''')
+        self.cur.execute(query)
+        return self.cur.fetchall()
+    
+    def woolies_all(self):
+        """
+        Select * From woolies_test
+        """
+        query = ('''
+            SELECT * FROM woolies_test
+            ''')
+        self.cur.execute(query)
+        return self.cur.fetchall()
